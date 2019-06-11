08c54f0ea73bc2e85a4596c87f8b83b02ac6e336
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-11 04:19:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 
 def hello_t(names)
+<<<<<<< HEAD
 if block_given?
  i = 0
 
@@ -10,5 +11,14 @@ end
  names
 else
   puts "Hey! No block was given!"
+=======
+ i = 0
+
+ while i < names.lenght
+   yield(names[i])
+  i += 1
+ end
+ names
+>>>>>>> f5d8a28e10d349b4d04e730e58cf35f42ec1474a
 end
 end
